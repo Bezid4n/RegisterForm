d9d45a4d84d9d4500f4a7e23fe6c917b68c93bd6
Bezid4n <bezid4n@yahoo.com>
2021-08-10 20:07:42 +0430

    designForm&Validate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,40 +10,51 @@ export class AppComponent implements OnInit {
 
   myForm = new FormGroup({
     personalData: new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
+      firstName: new FormControl(null,[Validators.required]),
+      lastName: new FormControl(null,[Validators.required]),
       birthDate: new FormControl()
     }),
 
     userData: new FormGroup({
-      userName: new FormControl(),
-      password: new FormControl(),
-      rePassword: new FormControl(),
-      email: new FormControl()
+      userName: new FormControl(null,[Validators.required]),
+      password: new FormControl(null,[Validators.required]),
+      rePassword: new FormControl(null,[Validators.required]),
+      email: new FormControl(null,[Validators.required, Validators.email])
 
     }),
 
     secretData: new FormGroup({
-      secretQuestion: new FormControl(),
-      secretAnswer: new FormControl(),
+      secretQuestion: new FormControl('team'),
+      secretAnswer: new FormControl(null,[Validators.required]),
     }),
 
-    contactData: new FormGroup({
-      tell: new FormArray([new FormGroup({
-        home: new FormControl(),
-        mobile: new FormControl(),
-        work: new FormControl()
-
-      })]),
       address: new FormArray([new FormGroup({
         city: new FormControl(),
         street: new FormControl(),
         alley: new FormControl(),
         no: new FormControl()
       })])
-    })
+
 
   })
+
+  submit(){
+    console.log(this.myForm)
+  }
+  reset(){
+    this.myForm.reset()
+  }
+
+  checkError(controlName: string, errorName: string){
+    let ctrl= this.myForm.get(controlName);
+    if(ctrl){
+      if(ctrl.invalid && ctrl.touched && ctrl.errors && ctrl.errors[errorName]) {
+        return true;
+      }
+    }
+    return false;
+
+  }
 
   constructor(){}
   ngOnInit(): void {
