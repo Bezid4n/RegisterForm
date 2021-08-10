1beff82ff0d48ff1fb626ab17ca59872d11313de
Bezid4n <bezid4n@yahoo.com>
2021-08-10 19:22:29 +0430

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  myForm = new FormGroup({
+    personalData: new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      birthDate: new FormControl()
+    }),
+
+    userData: new FormGroup({
+      userName: new FormControl(),
+      password: new FormControl(),
+      rePassword: new FormControl(),
+      email: new FormControl()
+
+    }),
+
+    secretData: new FormGroup({
+      secretQuestion: new FormControl(),
+      secretAnswer: new FormControl(),
+    }),
+
+    contactData: new FormGroup({
+      tell: new FormArray([new FormGroup({
+        home: new FormControl(),
+        mobile: new FormControl(),
+        work: new FormControl()
+
+      })]),
+      address: new FormArray([new FormGroup({
+        city: new FormControl(),
+        street: new FormControl(),
+        alley: new FormControl(),
+        no: new FormControl()
+      })])
+    })
+
+  })
+
+  constructor(){}
+  ngOnInit(): void {
+  }
   title = 'RegisterForm';
 }
